677cefe7c34d1c4347837c0fc92f4db5de67093d
Jean Jean <jean@MacBook-de-Jean-Jean.local>
2013-06-18 18:25:43 -0300

    Primeiro Commit

---
+++ listaC/listaC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  listaC
+//
+//  Created by Jean Jean on 18/06/13.
+//  Copyright (c) 2013 Jean Jean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "listaAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([listaAppDelegate class]));
+    }
+}
